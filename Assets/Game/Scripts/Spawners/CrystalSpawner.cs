0528962670c86446944b0cae007b6df6550aad2a
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2026-03-31 17:30:38 +0300

    Additions
     - Added crystal spawn system

---
+++ Assets/Game/Scripts/Spawners/CrystalSpawner.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpawner : SpawnerBase<Crystal>
+{
+    [SerializeField] private LayerMask _layerToSpawnOn;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _delay;
+
+    private WaitForSeconds _wait;
+
+    private float RandomPointOnAxis => Random.Range(-_radius, _radius);
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 center = transform.position;
+        Vector3 size = new Vector3(_radius, 0, _radius) * 2;
+
+        Gizmos.DrawWireCube(center, size);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _wait = new WaitForSeconds(_delay);
+
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (isActiveAndEnabled)
+        {
+            yield return _wait;
+
+            if (PoolSizeAtStart <= ActiveInPool)
+                continue;
+
+            Vector3 randomModifier = new Vector3 (RandomPointOnAxis, 0, RandomPointOnAxis);
+            Vector3 rayOrigin = transform.position + randomModifier;
+            Ray ray = new Ray(rayOrigin, Vector3.down);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, _layerToSpawnOn.value) == false)
+                continue;
+
+            Crystal crystal = ObjectPool.Get();
+            crystal.transform.position = hit.point;
+        }
+    }
+}
